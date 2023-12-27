67eaa35a8f3799324f341a0f8f64a25495fa0da2
Kalixto0701 <gabreilbnh2@gmail.com>
2023-12-27 14:28:43 -0300

    atualização

---
+++ index.js
@@ -0,0 +1,24 @@
+function ranked(vitorias, derrotas){
+    let saldo = vitorias - derrotas
+    let nivel;
+    
+        if(saldo < 10){
+          nivel = "Ferro"
+        }else if (saldo <= 20){
+          nivel = "Bronze"
+        }else if (saldo <= 50){
+          nivel = "Prata"
+        }else if (saldo <= 80){
+          nivel = "Ouro"
+        }else if (saldo <= 90){
+          nivel = "Diamante"
+        }else if (saldo <= 100){
+          nivel = "Lendário"
+        }else{
+          nivel = "Imortal"
+        }
+    
+        console.log("O Herói tem saldo de " + saldo + " e está no nível " + nivel)
+    }
+    
+    ranked(15, 5)
